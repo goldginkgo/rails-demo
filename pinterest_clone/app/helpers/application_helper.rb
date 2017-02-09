fb365ad31af6d50f594a209578cb7b96e78d283a
Frank Dai <yongjindai09@gmail.com>
2017-02-09 16:24:55 +1300

    Bootstrap for display messages

--- pinterest_clone/app/helpers/application_helper.rb
+++ pinterest_clone/app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def bootstrap_class_for(flash_type)
+    hash = HashWithIndifferentAccess.new(
+      success: 'alert-success',
+      error:   'alert-danger',
+      alert:   'alert-warning',
+      notice:  'alert-info'
+    )
+    hash[flash_type] || flash_type.to_s
+  end
 end
